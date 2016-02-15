100479f5cd571220e9bf35ed29fc1dfc2eb8782a
Thomas Beukema <thomasbeukema@outlook.com>
2016-02-15 20:18:24 +0100

    Added setup.py + readme

---
+++ Python/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='SOMToday API',
+    version='1.0',
+    packages=['hashlib', 'base64', 'requests', 'time', 'codecs'],
+    url='',
+    license='MIT',
+    author='Thomas Beukema',
+    author_email='thomasbeukema@outlook.com',
+    description='A Pythonic interface to the SOMToday API'
+)
---
+++ readme.md
@@ -0,0 +1,13 @@
+This is a repository for the SOMToday API. This is intended to be multilangual.
+
+Current supported languages are:
+	- Python
+
+Languages I am working on:
+	- Go
+	- Dart
+	- Java (Android)
+	- Rust
+	- PHP
+
+Happy coding!
