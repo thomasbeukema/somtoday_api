fe4a215fc4d7b9c7450ec88fe4a0200148463d3b
Thomas Beukema <thomasbeukema@outlook.com>
2016-02-15 19:59:31 +0100

    Fixed filesystem

---
+++ readme.txt
@@ -0,0 +1,4 @@
+This is an SOMToday API written in Python. This works with the mobile API from
+SOMToday. I did my best to make this api the simplest to use. You just have to
+import it in your Python file and you are ready to go! You can find an example
+script in "example.py". Happy coding!
--- somtoday.py
+++ somtoday.py
@@ -1,4 +1,12 @@
+"""
+    Author: Thomas Beukema
+    Email: thomasbeukema@outlook.com
+
+"""
+
 import hashlib, base64, requests, time, codecs
+
+# Just some custom exceptions, not that complicated ;)
 
 class FEATURE_NOT_ACTIVATED(Exception):
     pass
@@ -12,7 +20,10 @@ class FAILED_OTHER_TYPE(Exception):
 class SOMTODAY_ERROR_UNKNOWN(Exception):
     pass
 
+# Define the Somtoday class
 class Somtoday:
+
+    # Just declare some global variables
     leerlingID = None
     fullName = ""
 
@@ -39,13 +50,14 @@ class Somtoday:
         password = hashlib.sha1(credentials[1].encode("UTF-8")).digest() # Extract and hash password from tuple
         password = self.hex(base64.b64encode(password)) # Encode password
 
-        school   = credentials[2]
-        brin     = credentials[3]
+        school   = credentials[2] # Extract the schoolname from tuple
+        brin     = credentials[3] # Extract the BRIN code from tuple
 
-        loginURL = "http://somtoday.nl/" + school + "/services/mobile/v10/Login/CheckMultiLoginB64/" +  username + "/" + password + "/" + brin
+        loginURL = "http://somtoday.nl/" + school + "/services/mobile/v10/Login/CheckMultiLoginB64/" +  username + "/" + password + "/" + brin # Put the URL to login together
 
         response = self.getJSON(loginURL) # Perform request
 
+        # Check for different status codes. Raise error if status code nog right
         if response["error"] == "SUCCESS":
             self.leerlingID = str(response["leerlingen"][0]["leerlingId"])
             self.username = username
@@ -63,40 +75,45 @@ class Somtoday:
             raise SOMTODAY_ERROR_UNKNOWN("An unknown error has occured")
 
     def getTimetable(self, days = 0):
-        days = int(time.time() + (days * 86400)) * 1000
+        days = int(time.time() + (days * 86400)) * 1000 # Get many days in ms since epoch
 
+        # Put URL for timetable together
         URL = self.baseURL + "Agenda/GetMultiStudentAgendaB64/" + self.username + "/" + self.password + "/" + self.brin + "/" + str(days) + "/" + self.leerlingID
-        timetable = self.getJSON(URL)["data"]
+        timetable = self.getJSON(URL)["data"] # Request and extract data
 
         return timetable
 
     def getHW(self, daysMax = 31):
+        # Put URL for homework together
         URL = self.baseURL + "Agenda/GetMultiStudentAgendaHuiswerkMetMaxB64/" + self.username + "/" + self.password + "/" + self.brin + "/" + str(daysMax) + "/" + self.leerlingID
-        hw = self.getJSON(URL)["data"]
+        hw = self.getJSON(URL)["data"] # Request and extract data
 
         return hw
 
     def getGrades(self, mode = 1):
-
+        # Check if user wants just last 10 grades or all grades and then put URL together
         if mode == 0:
             URL = self.baseURL + "Cijfers/GetMultiCijfersRecentB64/" + self.username + "/" + self.password + "/" + self.brin + "/" + self.leerlingID
         elif mode == 1:
             URL = self.baseURL + "Cijfers/GetCijfersRecentMetMaxB64/" + self.username + "/" + self.password + "/" + self.brin + "/1000"
 
-        grades = self.getJSON(URL)["data"]
+        grades = self.getJSON(URL)["data"] # Request and extract data
         return grades
 
     def changeHwDone(self, appointmentID, hwID, done = True):
-
+        # Put URL for changing status of the item together
         URL = self.baseURL + "Agenda/HuiswerkDoneB64/" + self.username + "/" + self.password + "/" + self.brin + "/" + str(appointmentID) + "/" + str(hwID) + "/"
 
+        # Check which way user wants to check it
         if done:
             URL += "1"
         else:
             URL += "0"
 
-        response = self.getJSON(URL)
+        response = self.getJSON(URL) # Request data
 
+
+        # Parse data
         if response["status"] == "OK":
             return True
         else:
@@ -104,11 +121,12 @@ class Somtoday:
 
     @staticmethod
     def hex(toHex):
+        # Just a function to transform a string into hexadecimal
         return str(codecs.encode(toHex, "hex").decode())
 
     @staticmethod
     def getJSON(URL):
-        
+        # Get JSON from the server
         r = requests.get(URL)
         response = r.json()
 
